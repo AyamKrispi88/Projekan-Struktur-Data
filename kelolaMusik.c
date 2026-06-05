e68d1140c5a6dce13789da00e530b87e37349546
Krishna Ardi <baguskrishna52@gmail.com>
2026-06-05 22:15:53 +0800

    Ini baru ku tambahin ada itu di data sentral

--- kelolaMusik.c
+++ kelolaMusik.c
@@ -1,1 +1,20 @@
 /*Ini buat mengelola seluruh lagunya, buat ngeshow data lagu dari file txt musiks aja si*/
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct kelolaMusik
+{
+    char judul[1000];
+    char penyanyi[1000];
+
+    struct kelolaMusik *depan; // --> next
+    struct kelolaMusik *belakang; // double linked list --> prev
+}kelolaMusik;
+
+
+void liat(){
+    FILE *musik = fopen();
+}
--- user.c
+++ user.c
@@ -6,23 +6,31 @@
 typedef struct userData
 {
     char username[1000];
-    char password[8];
+    char password[100];
 }userData;
 
+void cekUsn(){
 
+}
 
 void registrasi(){
-
+    userData user;
+    printf("\nRegistrasi akun\n");
+    printf("Masukkan Username kamu: ");
+    fgets(user.username, sizeof(user.username), stdin);
+    user.username[strcspn(user.username, "\n")] = '\0';
+    printf("Masukkan passwordnya: ");
+    fgets(user.password, sizeof(user.password), stdin);
+    user.password[strcspn(user.password, "\n")] = '\0';
 }
 
 void login(){
     userData login;
+    FILE *user;
     printf("Masukkan Username kamu: ");
     fgets(login.username, sizeof(login.username), stdin);
     strcspn
     printf("Masukkan Password: ");
     fgets(login.password, sizeof(login.password), stdin);
     strcspn
-
-    
 }
