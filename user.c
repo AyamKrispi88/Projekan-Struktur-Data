7126beba958499116fe48871a453f59c8a19338d
Krishna Ardi <baguskrishna52@gmail.com>
2026-06-11 20:09:15 +0800

    Ini adalah commit baru di user

--- user.c
+++ user.c
@@ -3,11 +3,16 @@
 #include <string.h>
 #include <stdbool.h>
 
+void registrasi();
+int cekUsn(char *username);
+int login(char *username);
+
 typedef struct userData
 {
     char username[1000];
     char password[100];
 }userData;
+
 
 int cekUsn(char *username){
     char userUsn[100], userPw[100];
@@ -21,30 +26,86 @@ int cekUsn(char *username){
             return 1;
         }
     }
+
+    fclose(data);
+    return 0;
 }
 
 void registrasi(){
     userData user;
+    char pil;
+    FILE *dataUtama;
+    awalRegist:
     printf("\nRegistrasi akun\n");
-    printf("Masukkan Username kamu: ");
+    printf("Masukkan Username kamu(Gabisa diubah): ");
     fgets(user.username, sizeof(user.username), stdin);
     user.username[strcspn(user.username, "\n")] = '\0';
-    printf("Masukkan passwordnya: ");
+
+    if (cekUsn(user.username)){ //ini artinya, if fungsi ini true
+        printf("\nIni usernamenya udah ada, pake username lain\n");
+        printf("\nMau input username ulang?: ");
+        if (pil == 'y' || pil == 'Y'){
+            goto awalRegist;
+        } else if (pil == 'n' || pil == 'N'){
+            return;
+        } else {
+            printf("\nBenerin dikit lah inputnya\n");
+        }
+    }
+
+    printf("Masukkan passwordnya(Gabisa diubah): ");
     fgets(user.password, sizeof(user.password), stdin);
     user.password[strcspn(user.password, "\n")] = '\0';
 
-    if (cekUsn(user.username)){
-        printf("\nIni usernamenya udah ada, pake username lain\n");
+    dataUtama = fopen("DataSentral/user.txt", "a");
+    if (dataUtama == NULL){
+        printf("\nError, sistemnya gabisa jalan\n");
+        return;
     }
+
+    fprintf(dataUtama, "%s||%s\n", user.username, user.password);
+    fclose(dataUtama);
+    mkdir(user.username);
+
+    printf("Selamat datang %s", user.username);
+
 }
 
-void login(){
+int login(char *username){
     userData login;
     FILE *user;
+    char usn[100], userPw[100];
     printf("Masukkan Username kamu: ");
     fgets(login.username, sizeof(login.username), stdin);
-    strcspn
+    login.username[strcspn(login.username, "\n")] = '\0';
     printf("Masukkan Password: ");
     fgets(login.password, sizeof(login.password), stdin);
-    strcspn
+    login.password[strcspn(login.password, "\n")] = '\0';
+
+    char path;
+    sprintf(path, "DataSentral/user.txt");
+    user = fopen(path, "r");
+
+    while (user != EOF){
+
+        if (user == NULL){
+
+        }
+        (fscanf(user, "%s %s", usn, userPw));
+        if (strcm(username, usn) == 1){
+            if (strcmp(login.password, userPw) == 1){
+            }
+            if (strcmp(login.password, userPw) == 0){
+                printf("\nPassword atau Usernamenya salah, masukkan ulang\n");
+            }
+        } else {
+            printf("\nPassword atau usernamenya salah, masukkan ulang\n");
+        }
+    }
+    fclose(user);
+
+}
+
+void hapusAkun(char *username){
+
 }
